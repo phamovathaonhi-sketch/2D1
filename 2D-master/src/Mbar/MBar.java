4ac2c018d899ac57fdd5592817d059630e75fbaf
phamthaonhi <phamovathaonhi@gmail.com>
2026-05-21 10:36:46 +0200

    2105

--- 2D-master/src/Mbar/MBar.java
+++ 2D-master/src/Mbar/MBar.java
@@ -1,5 +1,6 @@
 package Mbar;
 
+import main.S1;
 import main.Selections;
 
 import javax.swing.*;
@@ -17,43 +18,47 @@ public class MBar extends JPanel {
     }
     public void init(){
         jFrame.setSize(200,320);
-        jFrame.setLocation(300,300);
+        jFrame.setLocation(490,490);
         jFrame.setBackground(new Color(197, 91, 91));
         jFrame.setVisible(true);
         jFrame.setResizable(false);
-        JButton HomeButton = createHomebutton(200-200/2,275);
-        HomeButton.addActionListener(e -> {
-            Selections selection = new Selections();
+        JButton HomeButton = createHomebutton(100,275);
+        /*HomeButton.addActionListener(e -> {
+            S1 s1 = new S1();
+            s1.startGamethread();
             jFrame.dispose();
+
         });
-        this.add(HomeButton);
+
+         */
+        jFrame.add(HomeButton);
     }
-    public JButton createHomebutton(int x, int y){
+    public JButton createHomebutton(int x, int y) {
         JButton homebutton = new JButton();
-        homebutton.setBounds(x,y, 100,40);
+        homebutton.setBounds(x, y, 50, 50);
         homebutton.setBorderPainted(false);
         homebutton.setContentAreaFilled(false);
         homebutton.setFocusPainted(false);
         homebutton.setOpaque(false);
         homebutton.setBorderPainted(false);
+        homebutton.setVisible(true);
 
-        URL imgURL = getClass().getResource("/Images/cake.png");
+      /*  URL imgURL = getClass().getResource("/Images/cpixil-frame-0 (4).png");
         if (imgURL != null) {
             ImageIcon temp = new ImageIcon(imgURL);
-            Image scaled = temp.getImage().getScaledInstance(180, 180, Image.SCALE_SMOOTH);
+            Image scaled = temp.getImage().getScaledInstance(100, 40, Image.SCALE_SMOOTH);
             homebutton.setIcon(new ImageIcon(scaled));
         }
+
+       */
         return homebutton;
     }
+
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(new Color(245, 210, 225));
-        g.fillRect(0, 0, getWidth(), getHeight());
-
-
+        g.fillRect(490, 490, 200,320);
     }
-
-
-
 }
